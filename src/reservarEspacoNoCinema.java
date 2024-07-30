c946a9a94234d12c08d2e53aac0b5dc31fe5d78b
Alexandre Brandão Santana <abrandaos@outlook.com>
2024-07-30 15:10:35 -0300

    @ABrandaoS

---
+++ src/reservarEspacoNoCinema.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class reservarEspacoNoCinema {
+    public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+
+        
+
+        leia.close();
+    }
+}
